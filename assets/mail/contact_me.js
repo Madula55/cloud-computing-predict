257a05b96dbc0759398b447c9ef2f8d295159a86
Andani Madula <andanimadula@Andanis-MBP-2.home>
2024-06-28 20:21:45 +0200

    add api

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "arn:aws:apigateway:eu-west-1::/apis/rmtnta4gpi/routes/dpq153p",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
