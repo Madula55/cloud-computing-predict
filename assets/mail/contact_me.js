af6f57cc1ffa37e96e7357f1469ebdeb080e4242
Andani Madula <andanimadula@Andanis-MBP-2.home>
2024-07-02 06:54:47 +0200

    add api1

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://fd3f134z4k.execute-api.eu-west-1.amazonaws.com",
+                url: "https://fd3f134z4k.execute-api.eu-west-1.amazonaws.com/stg",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
